7a87bd8212d886542f8e6efb2cbbb2a958460cc1
Edznux <edznux@gmail.com>
2018-06-18 15:34:59 +0200

    fix install

--- install-tools.sh
+++ install-tools.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #install-tools.sh
 
+DOTFILES_DIR="$HOME/dotfiles"
 INSTALL_DIR="$HOME/install"
 
 echo "Do you want to install tools ?"
@@ -31,6 +32,16 @@ install_pwngdb(){
     ./setup.sh
 }
 
+install_i3(){
+   echo "Installing i3"
+   sudo apt install i3 i3blocks
+   sudo ln -sf $DOTFILES_DIR/i3blocks.conf /etc/i3blocks.conf
+}
+
+# just in case, got time to ctrl + c
+sleep 3
+
+install_i3
 install_basic
 install_z
 install_pwngdb
